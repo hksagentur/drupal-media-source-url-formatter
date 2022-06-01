b475376b64aac3fb34f5bc322b350928acca420f
Daniel Weidner <d.weidner@hks-agentur.de>
2022-06-01 16:52:24 +0200

    test: use media kernel test base

--- tests/src/Kernel/FileUrlExtractorTest.php
+++ tests/src/Kernel/FileUrlExtractorTest.php
@@ -2,33 +2,25 @@
 
 namespace Drupal\Tests\media_source_url_formatter\Kernel;
 
-use Drupal\file\Entity\File;
-use Drupal\file\FileInterface;
-use Drupal\media\Entity\Media;
+use Drupal\Tests\media\Kernel\MediaKernelTestBase;
 
 /**
  * @group media_source_url_formatter
  */
-class FileUrlExtractorTest extends AbstractExtractorTestCase {
+class FileUrlExtractorTest extends MediaKernelTestBase {
 
   public function testFileUrlExtractor(): void {
+    /** @var \Drupal\media_source_url_formatter\FileUrlExtractor */
     $extractor = $this->container->get('media_source_url_formatter.file_url_extractor');
 
-    $test_files = $this->getTestFiles('binary');
+    /** @var \Drupal\media\MediaTypeInterface */
+    $media_type = $this->createMediaType('file');
 
-    $file = File::create([
-      'uri' => $test_files[0]->uri,
-      'uuid' => 'a2cb2b6f-7bf8-4da4-9de5-316e93487518',
-      'status' => FileInterface::STATUS_PERMANENT,
-    ]);
-    $file->save();
+    /** @var \Drupal\media\MediaInterface */
+    $media = $this->generateMedia('catalogue.pdf', $media_type);
 
-    $media = Media::create([
-      'bundle' => 'file',
-      'name' => 'File 1',
-      'field_media_file' => ['target_id' => $file->id()],
-    ]);
-    $media->save();
+    /** @var \Drupal\file\FileInterface */
+    $file = $media->field_media_file->first()->entity;
 
     $this->assertSame($file->createFileUrl(FALSE), $extractor->getUrl($media));
   }
